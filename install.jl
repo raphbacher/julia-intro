3387f5ee2f4757255c49cb42415751d6273ac1ef
Raphael Bacher <raphael.bacher@univ-grenoble-alpes.fr>
2019-05-28 08:43:59 +0200

    updates

--- install.jl
+++ install.jl
@@ -1,22 +1,4 @@
 using Pkg
-Pkg.add("IJulia")
-Pkg.add("DifferentialEquations")
-Pkg.add("Plots")
-Pkg.add("Interact")
-Pkg.add("Mux")
-Pkg.add("Blink")
-Pkg.add(PackageSpec(name="GenericSVD",rev="master"));
-Pkg.add("PyCall")
-Pkg.add("ForwardDiff")
-
 Pkg.activate(".")
-Pkg.add("IJulia")
-Pkg.add("DifferentialEquations")
-Pkg.add("Plots")
-Pkg.add("Interact")
-Pkg.add("Mux")
-Pkg.add("Blink")
-Pkg.add(PackageSpec(name="GenericSVD",rev="master"));
-Pkg.add("PyCall")
-Pkg.add("ForwardDiff")
-Pkg.activate()
+Pkg.instantiate()
+Pkg.precompile
